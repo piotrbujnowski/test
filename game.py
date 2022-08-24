42878449c32a660b3a86b1dac8bcb4625193c6ee
martyna-marcinko <martyna.marcinko@gmail.com>
2022-08-24 14:26:52 +0200

    dssdf

--- game.py
+++ game.py
@@ -1,2 +1,18 @@
-print ("paper, rock, scissors")
-qefndvs
+import random
+
+
+choices = ["Rock", "Paper", "Scissors"]
+computer = random.choice(choices)
+
+print("Write rock, paper or scissors")
+   choice= int(input)()
+    if input=("rock") and computer = "rock"
+
+#variable_1 = "paper"
+#variable_2 = "rock" 
+#variable_3 = "scissors"
+
+#input(1, 2, 3)
+
+
+   # print (remis)
