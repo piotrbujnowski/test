5139a7793c454d427141b35892c1a7898e59ad5b
martyna-marcinko <martyna.marcinko@gmail.com>
2022-08-25 11:42:59 +0200

    komentarzzz

---
+++ password.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+def pass_gen(size = 8, chars=string.ascii_letters + string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation):
+
+    return ''.join(random.choice(chars) for _ in range(size))
+
+pass_length = int(input("Your password's length: "))
+print(pass_gen(pass_length))
